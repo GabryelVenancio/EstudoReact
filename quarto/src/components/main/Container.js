fb07d1d85a78672640a6b7aea20b11bb0147d327
GABRYEL VENANCIO CLEFFS DO NASCIMENTO <gabryel.vcnascimento@senacsp.edu.br>
2022-05-17 17:20:21 -0300

    Atualização da api

--- quarto/src/components/main/Container.js
+++ quarto/src/components/main/Container.js
@@ -1,11 +1,43 @@
+import {useState,useEffect} from "react";
 import Conteudo from "./Conteudo";
 import Mensagem from "./Mensagem";
 
 export default function Container() {
+
+
+    //vamos criar o estado inicial dos produtos, ou seja,
+    //quais dados existem dentro do objeto produtos
+    const [produtos, setProdutos] =useState([
+        {
+            id:"",
+            nome:"",
+            descricao:"",
+            categoria:"",
+            preco:"",
+            msg:[]
+        }
+    ]);
+
+    useEffect(()=>{
+
+        fetch("http://10.26.49.26:4000/api/services/produto/listar")
+        .then((response)=>response.json())
+        .then((dados)=>{
+            setProdutos(dados.output);
+        })
+        .catch((erro)=>console.error(`Erro ao carregar a api ->${erro}`))
+        
+    },[])
+
+
+
+
+
+
     return (
         <div className="container">
             <Mensagem/>
-            <Conteudo/>
+            <Conteudo dados = {produtos}/>
         </div>
     )
 }
